72ec06c2a1aa03a5386bb127bec9c4d8f3805233
Brad Collette <bradcollette@pop-os.localdomain>
2023-02-02 22:24:12 +0100

    Deploy website - based on 7c5442f86bf046a0299257d1e8fcf6020a2f06d1

--- assets/js/7d7fd0cd.6ca79b10.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkondsel=self.webpackChunkondsel||[]).push([[991],{1632:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/news","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
