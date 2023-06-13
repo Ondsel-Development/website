07b8380d1304c5ef72c9edade91378432598b7dc
sliptonic <shopinthewoods@gmail.com>
2023-06-13 16:28:31 -0500

    setting up for automatic deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ondsel', // Usually your GitHub org/user name.
+  organizationName: 'Ondsel-Development', // Usually your GitHub org/user name.
   projectName: 'website', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
