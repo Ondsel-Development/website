3011f7deb017fc9785975b4035272feb86d7e4ae
Brad Collette <bradcollette@pop-os.localdomain>
2023-07-25 10:54:06 -0500

    interim changes

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -6,99 +6,34 @@ import PricingCardList from "@site/src/components/PricingCardList";
 
 const PRICING_PLANS = [
   {
-    name: "Anonymous",
+    name: "Community",
     slug: "plan-anonymous",
     price: "$0",
     bgColor: "#5E332A",
     cta: "notify",
     features: [
+      { id: "pfeature-core-freecad",
+        title: "Full FreeCAD Application",
+        status: "completed",
+      },
       {
         id: "pfeature-local-workspaces",
         title: "Local Workspaces",
         status: "completed",
       },
       {
-        id: "pfeature-upload-visualize",
-        title: "Explore public models",
-        status: "completed",
-      },
-      {
-        id: "pfeature-download",
-        title: "Download in original format",
-        status: "completed",
-      },
-      // {
-      //   id: "pfeature-disappear-after-24h",
-      //   title: "Disappear after 24 hours.",
-      //   status: "completed",
-      // },
-      {
         id: "pfeature-lens",
-        title: "Ondsel Lens (Online viewer for 3D models)",
-        status: "pending",
+        title: "Ondsel Lens Addon",
+        status: "completed",
       },
-      // {
-      //   id: "pfeature-upload-webpage",
-      //   title: "Upload from webpage",
-      //   status: "completed",
-      // },
+      {
+        id: "pfeature-50-models",
+        title: "50 models (public)",
+        status: "completed",
+      },
     ],
   },
-  {
-    name: "Authenticated",
-    slug: "plan-authenitcated",
-    price: "$0",
-    bgColor: "#4F2A22",
-    cta: "signup",
-    features: [
-      {
-        id: "pfeature-5-models",
-        title: "5 models (public)",
-        status: "completed",
-      },
-      // {
-      //   id: "pfeature-public-view",
-      //   title: "Publicly viewable",
-      //   status: "completed",
-      // },
-      {
-        id: "pfeature-reader-controllable-param",
-        title: "Customize Model in Browser",
-        status: "completed",
-      },
-      {
-        id: "pfeature-upload-webpage-workbench",
-        title: "Upload from webpage or workbench",
-        status: "completed",
-      },
-      {
-        id: "pfeature-download-formats",
-        title: "Download in alternate formats",
-        status: "completed",
-      },
-      {
-        id: "pfeature-ray-trace",
-        title: "Ray traced image rendering",
-        status: "pending",
-      },
-      // {
-      //   id: "pfeature-path-setupbook",
-      //   title: "Path Setupbook",
-      //   status: "completed",
-      // },
-      {
-        id: "pfeature-techdraw-pages",
-        title: "Techdraw pages",
-        status: "pending",
-      },
-      // {
-      //   id: "pfeature-model-catalog",
-      //   title: "Model catalog",
-      //   status: "completed",
-      // },
-    ],
-  },
-  {
+    {
     name: "Premium",
     slug: "plan-premium",
     price: "$10 / Month",
@@ -106,8 +41,8 @@ const PRICING_PLANS = [
     cta: "buy-now",
     features: [
       {
-        id: "pfeature-50-models",
-        title: "50 models",
+        id: "pfeature-250-models",
+        title: "250 models",
         status: "completed",
       },
       {
@@ -124,6 +59,11 @@ const PRICING_PLANS = [
         id: "pfeature-control-download-formats",
         title: "Control download formats",
         status: "completed",
+      },
+      {
+        id: "pfeature-multiple-workspaces",
+        title: "multiple shareable workspaces",
+        status: "pending",
       },
       {
         id: "pfeature-versions",
@@ -145,8 +85,8 @@ const PRICING_PLANS = [
     cta: "contact-us",
     features: [
       {
-        id: "pfeature-dedicated-server",
-        title: "Dedicated server",
+        id: "pfeature-org-workspaces",
+        title: "Organizational Workspaces",
         status: "pending",
       },
       {
@@ -155,8 +95,8 @@ const PRICING_PLANS = [
         status: "pending",
       },
       {
-        id: "pfeature-multiple-workspaces",
-        title: "Shared Workspaces",
+        id: "pfeature-shareable-workspaces",
+        title: "Shareable Workspaces",
         status: "pending",
       },
       {
