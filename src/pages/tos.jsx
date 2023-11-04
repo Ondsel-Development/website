de790cc9fe1dcee00aecb29b4c7081ea25072f61
Pat David <patdavid@gmail.com>
2023-11-04 10:23:48 -0500

    TOS: add TOS component and page
    This adds a page `tos.jsx` to fetch the TOS from the API
    (https://lens-api.ondsel.com/agreements?category=terms-of-service)
    
    I had to add a dependency `react-markdown` to parse the markdown
    data returned.
    
    I'll implement the same for the privacy policy if this passes
    muster. :)

---
+++ src/pages/tos.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import ReactDom from 'react-dom';
+import Markdown from 'react-markdown';
+
+import Layout from "@theme/Layout";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import MainWrapper from "@site/src/components/MainWrapper";
+
+
+export default function TOS() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchTOS = async () => {
+
+    try {
+      fetch('https://lens-api.ondsel.com/agreements?category=terms-of-service')
+        .then( (response) => {
+          if( response.ok ) {
+            return response.json();
+          }
+        })
+        .then( (responseJSON) => {
+          const mdContent = responseJSON.data[0].current.markdownContent;
+          setData( mdContent );
+          setLoading(false);
+        });
+    } catch (err) {
+      setError( err.message );
+    }
+  }
+
+  useEffect( () => {
+    fetchTOS();
+  }, [] );
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <Layout title="Terms of Service" description={siteConfig.tagline}>
+      <MainWrapper className="container container--fluid margin-ver--lg row--align-center">
+          <div className='col col--8'>
+          <Markdown>{data}</Markdown>;
+          </div>
+      </MainWrapper>
+    </Layout>
+  );
+}
