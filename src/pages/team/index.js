8498fc3b421591028d9fdfe820dfdacef4524653
Pat David <patdavid@gmail.com>
2023-11-05 09:54:21 -0600

    team: move person file to mdx

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -5,7 +5,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import MainWrapper from "@site/src/components/MainWrapper";
 import ContactFormSection from "@site/src/components/ContactFormSection";
 
-import Pat from './pat-david.md'
+import Pat from './_pat-david.mdx'
 
 
 export default function Team() {
@@ -14,14 +14,9 @@ export default function Team() {
 
   return (
     <Layout title="Team" description={siteConfig.tagline}>
-      <MainWrapper className="bg-grid-pattern px_8">
+      <MainWrapper className="px_8">
         <h1 className="text_6xl text_gray text_center">Who We Are</h1>
-        {/* <h2 className="text_2xl text_center text_gray"> */}
-        {/*   Sign up to be invited to the beta program */}
-        {/* </h2> */}
-        {/* <ContactFormSection /> */}
         <Pat>
-          {metadata.title}
         </Pat>
       </MainWrapper>
     </Layout>
