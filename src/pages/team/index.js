2493c1545fd2175c65f29491fe185ec7ff1ea143
Pat David <patdavid@gmail.com>
2023-11-07 21:53:31 -0600

    team: {children} - duh.  styling team member

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -3,21 +3,21 @@ import React from "react";
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import MainWrapper from "@site/src/components/MainWrapper";
-import ContactFormSection from "@site/src/components/ContactFormSection";
 
-import Pat from './_pat-david.mdx'
+import Pat from "./_pat-david.mdx"
 
 
 export default function Team() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
 
-  return (
+ return (
     <Layout title="Team" description={siteConfig.tagline}>
-      <MainWrapper className="px_8">
-        <h1 className="text_6xl text_gray text_center">Who We Are</h1>
-        <Pat>
-        </Pat>
+      <MainWrapper className="container container--fluid row--align-center">
+        <h1 className="text_6xl text_center">Who We Are</h1>
+          <section className="col">
+            <Pat/>
+          </section>
       </MainWrapper>
     </Layout>
   );
