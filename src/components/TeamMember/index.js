3804202c4d11044a89ae5c6b5e0ff83804aca887
Pat David <patdavid@gmail.com>
2023-11-09 21:23:52 -0600

    team: move member to component, start styling
    This moves the team member component out of the .mdx file
    and into a separate component file.
    
    Pass the frontMatter to the component `<TeamMember>`.
    
    Start styling the component.
    (Wish Infima had a more mature grid responsive system).

---
+++ src/components/TeamMember/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+
+export default function TeamMember({children, frontMatter}) {
+  const photo = require( '@site/src/pages/team/' + frontMatter.photo ).default;
+  console.log('in TeamMember');
+  return (
+    <div className={'flex flex_row ' + styles.team_member}>
+      <img className={ styles.member_photo } src={ photo } />
+      <div className={ 'flex flex_col member-info '+ styles.member_info }>
+        <h2 className={ styles.member_name }>{frontMatter.name}</h2>
+        <span className={ styles.member_email }>{frontMatter.email}</span>
+        <span className={ styles.member_twitter }>{frontMatter.twitter}</span>
+        <div className={ styles.member_text }>
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
