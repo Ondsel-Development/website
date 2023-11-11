ada0b2c752219f51a1640debb1b9041be0534288
Pat David <patdavid@gmail.com>
2023-11-10 21:47:49 -0600

    team: style team member component
    This styles the team member component and swaps out some
    styles in favor of infima container/row/cols to be responsive.

--- src/components/TeamMember/index.js
+++ src/components/TeamMember/index.js
@@ -6,15 +6,21 @@ export default function TeamMember({children, frontMatter}) {
   const photo = require( '@site/src/pages/team/' + frontMatter.photo ).default;
   console.log('in TeamMember');
   return (
-    <div className={'flex flex_row ' + styles.team_member}>
-      <img className={ styles.member_photo } src={ photo } />
-      <div className={ 'flex flex_col member-info '+ styles.member_info }>
+    <div className={'row ' + styles.team_member}>
+
+      <div className='col col--3 '>
+        <img className={ styles.member_photo } src={ photo } />
+      </div>
+
+      <div className={ 'col  '+ styles.member_info }>
         <h2 className={ styles.member_name }>{frontMatter.name}</h2>
           { frontMatter.title
             && <h3 className={ styles.member_title }>{frontMatter.title}</h3> }
-          <div>
-          <span className={ styles.member_email }>{frontMatter.email}</span> 
-          <span className={ styles.member_twitter }>{frontMatter.twitter}</span>
+          <div className={styles.member_contact}>
+            <span className={ styles.member_email }>
+              <a href={frontMatter.email}>{frontMatter.email}</a>
+            </span> 
+            <span className={ styles.member_twitter }>{frontMatter.twitter}</span>
           </div>
         <div className={ styles.member_text }>
           {children}
