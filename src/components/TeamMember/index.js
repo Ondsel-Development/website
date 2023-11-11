84bf4246ec1633d9010a4236ccd9befca9dcce32
Pat David <patdavid@gmail.com>
2023-11-10 20:57:13 -0600

    team: start styling team member card
    Need to make it responsive via infima container/row/cols.

--- src/components/TeamMember/index.js
+++ src/components/TeamMember/index.js
@@ -10,8 +10,12 @@ export default function TeamMember({children, frontMatter}) {
       <img className={ styles.member_photo } src={ photo } />
       <div className={ 'flex flex_col member-info '+ styles.member_info }>
         <h2 className={ styles.member_name }>{frontMatter.name}</h2>
-        <span className={ styles.member_email }>{frontMatter.email}</span>
-        <span className={ styles.member_twitter }>{frontMatter.twitter}</span>
+          { frontMatter.title
+            && <h3 className={ styles.member_title }>{frontMatter.title}</h3> }
+          <div>
+          <span className={ styles.member_email }>{frontMatter.email}</span> 
+          <span className={ styles.member_twitter }>{frontMatter.twitter}</span>
+          </div>
         <div className={ styles.member_text }>
           {children}
         </div>
