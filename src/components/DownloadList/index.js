1ffb8a165fb2c6b8216682999ce5d75bf1244d41
Pat David <patdavid@gmail.com>
2023-12-07 10:37:43 -0600

    download: using dummy latest.json, hacking at page
    Starting to hack at a layout for downloads page.
    Using a dummy latest.json to avoid GH API rate limits.

--- src/components/DownloadList/index.js
+++ src/components/DownloadList/index.js
@@ -3,19 +3,11 @@ import React from "react";
 import styles from "./styles.module.css";
 
 export default function DownloadList({ assets }) {
-  console.log( 'Inside DownloadList' );
-  console.log( '(DownloadList) assets: ' );
-  console.log( typeof(assets) );
-  console.log( assets );
   return (
     <>
-      {/* <div className={styles.pricingCardList}> */}
-      {/*   {plans.map((plan) => ( */}
-      {/*     <PricingCard key={plan.slug} plan={plan} /> */}
-      {/*   ))} */}
-      {/* </div> */}
-      <div>DL List</div>
       <div>
+        <h3>Testing</h3>
+        {assets && assets.map( (e) => (<li>{e.url}</li>) )}
       </div>
     </>
   );
--- src/pages/download.jsx
+++ src/pages/download.jsx
@@ -13,10 +13,12 @@ export default function Download() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [isBusy, setBusy] = useState(true);
 
 
   const fetchDownloads = async () => {
     try{
+    fetch("latest.json")
       fetch('https://api.github.com/repos/Ondsel-Development/FreeCAD/releases/latest')
         .then( (response) => {
           if( response.ok ) {
@@ -31,6 +33,7 @@ export default function Download() {
           console.log( responseJSON );
           console.log('responseJSON.assets:');
           setData( responseJSON.assets );
+          setBusy(false);
         });
     } catch (err) {
       console.log( err.message );
@@ -47,12 +50,45 @@ export default function Download() {
   return (
     <Layout title="Download" description={siteConfig.tagline}>
       <MainWrapper className="bg-grid-pattern px_8">
-        <h1 className="text_6xl text_gray text_center">Download</h1>
-        <h2 className="text_2xl text_center text_gray">
+        <div class='container'>
+          <h1 className="text_6xl text_gray text_center">Download</h1>
+          <h2 className="text_2xl text_center text_gray">
           Put your designs to work everywhere
-        </h2>
-        <div>
-          <DownloadList assets={data} />
+          </h2>
+          <div class='row text_gray'>
+
+            <div class='col col--6'>
+              <div class="dropdown dropdown--hoverable">
+                <button class="button button--lg button--primary">Download</button>
+                <ul class="dropdown__menu">
+                  <li>
+                    <a class="dropdown__link" href="#url">Home</a>
+                  </li>
+                  <li>
+                    <a class="dropdown__link" href="#url">Profile</a>
+                  </li>
+                  <li>
+                    <a class="dropdown__link" href="#url">Settings</a>
+                  </li>
+                  <li>
+                    <a class="dropdown__link" href="#url">Help</a>
+                  </li>
+                  <li>
+                    <a class="dropdown__link" href="#url">Logout</a>
+                  </li>
+                </ul>
+              </div>
+
+              {isBusy ? 'Loading' : <DownloadList assets={data} /> }
+            </div>
+
+            <div class='col col--6'>
+              <div>The lastest version is: {isBusy ? '' : data[0].id }</div>
+            </div>
+
+          {/* {isBusy ? 'Loading' : data.map( (e) => (e.name) ) } */}
+
+          </div>
         </div>
       </MainWrapper>
     </Layout>
