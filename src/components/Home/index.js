5b05131c43dc2a297013682fe0f32742e00e84a4
Pat David <patdavid@gmail.com>
2024-01-12 22:57:07 -0600

    home: start implementing mockup per prokoudine

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomePageFeatures from "@site/src/components/HomePageFeatures";
 import TwoColumnInfo from "@site/src/components/TwoColumnInfo";
+import YouTube from "@site/src/components/YouTube";
 
 import styles from "./index.module.css";
 
@@ -47,6 +48,38 @@ function HomePageHeader() {
   );
 }
 
+function NewHeader() {
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+      <header className={clsx("hero hero--primary bg-grid-pattern", styles.heroBanner)}>
+        <div className='container'>
+          <div className='row' style={{padding: '5rem 0'}}>
+
+            <div className='col col-6' style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+              <p className='text_hero_img' style={{textAlign: 'left', lineHeight: '1.2'}}>
+                Collaborate with open-source engineering tools
+              </p>
+              <div className={styles.buttons} style={{marginTop: '1rem', justifyContent: 'start'}}>
+                <Link
+                  className="button button--secondary button--lg text_hero"
+                  to="/download"
+                >
+                  Download
+                </Link>
+              </div>
+            </div>
+
+            <div className='col col-6'>
+              <YouTube v='MI2JMm__bkM' style={{borderRadius: '1.5rem'}}/>
+            </div>
+
+          </div>
+        </div>
+    </header>
+  );
+}
+
 export default function Home({ recentPosts }) {
   const { siteConfig } = useDocusaurusContext();
 
@@ -56,6 +89,34 @@ export default function Home({ recentPosts }) {
       description={siteConfig.tagline}
     >
       <HomePageHeader />
+      <NewHeader/>
+
+      <section className={clsx("hero hero--primary ", styles.heroBanner)} style={{background: 'white', color: 'black'}}>
+        <div className='container'>
+          <div className='row' style={{padding: '5rem 0'}}>
+
+            <div className='col col-6' style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+              <p className='text_hero_img' style={{textAlign: 'left', lineHeight: '1.2'}}>
+                Access and visualize your 3D CAD models anywhere
+              </p>
+              <div className={styles.buttons} style={{marginTop: '1rem', justifyContent: 'start'}}>
+                <Link
+                  className="button button--primary button--lg text_hero"
+                  to="/download"
+                >
+                  Sign Up
+                </Link>
+              </div>
+            </div>
+
+            <div className='col col-6'>
+              <img src='/img/ondsel-access.png' className='shadow--md' style={{borderRadius: '1.5rem', border: 'solid 1px gray'}}/>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
       <main>
         <HomePageFeatures />
         <TwoColumnInfo data={recentPosts} />
