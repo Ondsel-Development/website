04799f95f4d6484fb5f96f0cb7bb876bfcaa7115
Pat David <patdavid@gmail.com>
2024-03-02 21:52:59 -0600

    download: add component and logic for pre-releases
    This parses the full release JSON file from GH and parses
    according to current release or pre-release.
    
    New component added for showing pre-release options.

--- src/pages/download.jsx
+++ src/pages/download.jsx
@@ -4,12 +4,30 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import MainWrapper from "@site/src/components/MainWrapper";
 
 import DownloadList from "@site/src/components/DownloadList";
+import DownloadListPrerelease from "@site/src/components/DownloadListPrerelease";
+
+
+function returnData( d, release ){
+
+  let request;
+
+  if (release === 'release'){
+    request = d.filter( el => (el.draft === false && el.prerelease === false) )[0];
+  }else if( release === 'prerelease' ){
+    request = d.filter( el => (el.draft === false && el.prerelease === true) )[0];
+  }
+
+  return request
+
+}
+
 
 export default function Download() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
 
   const [data, setData] = useState(null);
+  const [prerelease, setPrerelease] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [isBusy, setBusy] = useState(true);
@@ -17,8 +35,9 @@ export default function Download() {
 
   const fetchDownloads = async () => {
     try{
-    fetch("latest.json")
-      fetch('https://api.github.com/repos/Ondsel-Development/FreeCAD/releases/latest')
+    // fetch("latest.json")
+      // fetch('https://api.github.com/repos/Ondsel-Development/FreeCAD/releases/latest')
+      fetch('https://api.github.com/repos/Ondsel-Development/FreeCAD/releases')
         .then( (response) => {
           if( response.ok ) {
             // console.log( 'response.ok' );
@@ -30,8 +49,11 @@ export default function Download() {
         .then( (responseJSON) => {
           // console.log( 'responsejSON: ' );
           // console.log( responseJSON );
-          // console.log('responseJSON.assets:');
-          setData( responseJSON );
+          // const tmpData = returnData( responseJSON, 'release' );
+          
+          // setData( responseJSON );
+          setData( returnData( responseJSON, 'release' ));
+          setPrerelease( returnData( responseJSON, 'prerelease' ));
           setBusy(false);
         });
     } catch (err) {
@@ -69,52 +91,54 @@ export default function Download() {
           {/* Pre-Release downloads section */}
           <div className='row text_gray margin-vert--lg' style={{justifyContent: 'center', textAlign: 'center'}}>
             <div className='col col--8'>
-              <h2>Pre-releases</h2>
+              <h2 class='text_4xl'>Pre-releases</h2>
                 <div className='unstable show'>
-                  <p>The pre-release version of Ondsel is: XXX.XXX</p>
-                  <p>This is a blurb about what the unstable release is and that it shouldn't be used for normal production work.  Include the necessary caveats.</p>
+                  <p>The latest pre-release version of Ondsel was built on {isBusy ? '' : prerelease.created_at}</p>
+                  <p>⚠️ This is a prerelease. It shouldn't be used for normal production work. ⚠️ </p>
 
-                  <div class="dropdown dropdown--hoverable">
-                    <button class="button button--lg button--outline button--primary margin-horiz--sm margin-bottom--md" style={{color: 'white'}}>
-                      <img src="/img/os_linux.svg" class="os_icon_src-components-DownloadList-styles-module margin-right--sm" style={{filter: 'invert(1)', display: 'inline', verticalAlign: 'inherit'}}/>
-                      Linux <small style={{color: 'gray', fontWeight: '400'}}>[Pre-release]</small> 
-                    </button>
-                    <ul class="dropdown__menu">
-                      <li>
-                        <a class="dropdown__link" href="#url">aarch.AppImage</a>
-                      </li>
-                      <li>
-                        <a class="dropdown__link" href="#url">x86_64.AppImage</a>
-                      </li>
-                    </ul>
-                  </div>
+                  {isBusy ? 'Loading' : <DownloadListPrerelease assets={prerelease.assets} /> }
 
-                  <div class="dropdown dropdown--hoverable">
-                    <button class="button button--lg button--outline button--primary margin-horiz--sm margin-bottom--md" style={{color: 'white'}}>
-                    <img src="/img/os_mac.svg" class="os_icon_src-components-DownloadList-styles-module margin-right--sm" style={{filter: 'invert(1)', display: 'inline', verticalAlign: 'inherit'}}/>
-                      macOS <small style={{color: 'gray', fontWeight: '400'}}>[Pre-release]</small>
-                    </button>
-                    <ul class="dropdown__menu">
-                      <li>
-                        <a class="dropdown__link" href="#url">arm64.dmg</a>
-                      </li>
-                      <li>
-                        <a class="dropdown__link" href="#url">x86_64.dmg</a>
-                      </li>
-                    </ul>
-                  </div>
+                  {/* <div class="dropdown dropdown--hoverable"> */}
+                  {/*   <button class="button button--lg button--outline button--primary margin-horiz--sm margin-bottom--md" style={{color: 'white'}}> */}
+                  {/*     <img src="/img/os_linux.svg" class="os_icon_src-components-DownloadList-styles-module margin-right--sm" style={{filter: 'invert(1)', display: 'inline', verticalAlign: 'inherit'}}/> */}
+                  {/*     Linux <small style={{color: 'gray', fontWeight: '400'}}>[Pre-release]</small> */} 
+                  {/*   </button> */}
+                  {/*   <ul class="dropdown__menu"> */}
+                  {/*     <li> */}
+                  {/*       <a class="dropdown__link" href="#url">aarch.AppImage</a> */}
+                  {/*     </li> */}
+                  {/*     <li> */}
+                  {/*       <a class="dropdown__link" href="#url">x86_64.AppImage</a> */}
+                  {/*     </li> */}
+                  {/*   </ul> */}
+                  {/* </div> */}
 
-                  <div class="dropdown dropdown--hoverable">
-                    <button class="button button--lg button--outline button--primary margin-horiz--sm margin-horiz--md margin-bottom--md" style={{color: 'white'}}>
-                    <img src="/img/os_mac.svg" class="os_icon_src-components-DownloadList-styles-module margin-right--sm" style={{filter: 'invert(1)', display: 'inline', verticalAlign: 'inherit'}}/>
-                      Windows <small style={{color: 'gray', fontWeight: '400'}}>[Pre-release]</small>
-                    </button>
-                    <ul class="dropdown__menu">
-                      <li>
-                        <a class="dropdown__link" href="#url">x86_64.7z</a>
-                      </li>
-                    </ul>
-                  </div>
+                  {/* <div class="dropdown dropdown--hoverable"> */}
+                  {/*   <button class="button button--lg button--outline button--primary margin-horiz--sm margin-bottom--md" style={{color: 'white'}}> */}
+                  {/*   <img src="/img/os_mac.svg" class="os_icon_src-components-DownloadList-styles-module margin-right--sm" style={{filter: 'invert(1)', display: 'inline', verticalAlign: 'inherit'}}/> */}
+                  {/*     macOS <small style={{color: 'gray', fontWeight: '400'}}>[Pre-release]</small> */}
+                  {/*   </button> */}
+                  {/*   <ul class="dropdown__menu"> */}
+                  {/*     <li> */}
+                  {/*       <a class="dropdown__link" href="#url">arm64.dmg</a> */}
+                  {/*     </li> */}
+                  {/*     <li> */}
+                  {/*       <a class="dropdown__link" href="#url">x86_64.dmg</a> */}
+                  {/*     </li> */}
+                  {/*   </ul> */}
+                  {/* </div> */}
+
+                  {/* <div class="dropdown dropdown--hoverable"> */}
+                  {/*   <button class="button button--lg button--outline button--primary margin-horiz--sm margin-horiz--md margin-bottom--md" style={{color: 'white'}}> */}
+                  {/*   <img src="/img/os_mac.svg" class="os_icon_src-components-DownloadList-styles-module margin-right--sm" style={{filter: 'invert(1)', display: 'inline', verticalAlign: 'inherit'}}/> */}
+                  {/*     Windows <small style={{color: 'gray', fontWeight: '400'}}>[Pre-release]</small> */}
+                  {/*   </button> */}
+                  {/*   <ul class="dropdown__menu"> */}
+                  {/*     <li> */}
+                  {/*       <a class="dropdown__link" href="#url">x86_64.7z</a> */}
+                  {/*     </li> */}
+                  {/*   </ul> */}
+                  {/* </div> */}
 
                 </div>
 
