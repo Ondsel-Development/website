d396292c3a3b404156c0d450a97a39e0d8213205
Pat David <patdavid@gmail.com>
2024-09-09 22:06:48 -0500

    Blog: add archive, tag link under sidebar
    Part of addressing #18 for user browsing blog content

---
+++ src/theme/BlogSidebar/Desktop/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import {translate} from '@docusaurus/Translate';
+import styles from './styles.module.css';
+export default function BlogSidebarDesktop({sidebar}) {
+  return (
+    <aside className="col col--3">
+      <nav
+        className={clsx(styles.sidebar, 'thin-scrollbar')}
+        aria-label={translate({
+          id: 'theme.blog.sidebar.navAriaLabel',
+          message: 'Blog recent posts navigation',
+          description: 'The ARIA label for recent posts in the blog sidebar',
+        })}>
+        <div className={clsx(styles.sidebarItemTitle, 'margin-bottom--md')}>
+          {sidebar.title}
+        </div>
+        <ul className={clsx(styles.sidebarItemList, 'clean-list')}>
+          {sidebar.items.map((item) => (
+            <li key={item.permalink} className={styles.sidebarItem}>
+              <Link
+                isNavLink
+                to={item.permalink}
+                className={styles.sidebarItemLink}
+                activeClassName={styles.sidebarItemLinkActive}>
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <hr style={{marginTop: 0, marginBottom: '1rem'}}/>
+        <Link to="/blog/archive" className={styles.bold}>Archive</Link><br/>
+        <Link to="/blog/tags" className={styles.bold}>Tags</Link>
+
+      </nav>
+    </aside>
+  );
+}
